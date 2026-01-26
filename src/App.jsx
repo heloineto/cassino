4443e45ef78d9d1f5cee28d1a1da2ae06aba21c1
Heloi Neto <hey@heloineto.com>
2026-01-26 13:29:45 -0300

    fiz algo

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,24 @@ export function App() {
 
   let [dinheiro, setDinheiro] = useState(1800);
   let [resultado, setResultado] = useState();
-  const optionsWheel = [
-    -1800, 2000, -400, -2400, 5000, -10000, -3000, 1000, -11000, 50000, 10,
-    -30000, 1000000,
-  ];
 
-  const handleSpinEnd = (sector) => {
-    console.log('Winner:', sector.label, sector);
-  };
+  const sectors=[
+    { id: 1, label: 'Perca R$1800', value: -1_800, probability: 10 },
+    { id: 2, label: 'Ganhe R$2000', value: 2000, probability: 20 },
+    { id: 3, label: 'Perca R$400', value: -400, probability: 15 },
+    { id: 4, label: 'Ganhe R$5000', value: 5_000, probability: 5 },
+    { id: 5, label: 'Perca R$10000', value: -100_000, probability: 5 },
+    { id: 6, label: 'Ganhe R$1000', value: 100, probability: 5 },
+    { id: 7, label: 'Ganhe R$3000', value: -3000, probability: 5 },
+    { id: 8, label: 'Perca R$11000', value: -11_000, probability: 5 },
+    { id: 9, label: 'Ganhe R$50000', value: 50_000, probability: 5 },
+    { id: 10, label: 'Perca R$11000', value: -11_000, probability: 5 },
+    { id: 11, label: 'Ganhe R$10', value: 10, probability: 5 },
+    { id: 12, label: 'Perca R$30000', value: -30_000, probability: 5 },
+    { id: 13, label: 'Ganhe R$10', value: -1_000_000, probability: 100 },
+    { id: 14, label: 'Ganhe R$1,00', value: -100, probability: 5 },
+
+  ];
 
   return (
     <div>
@@ -22,36 +32,50 @@ export function App() {
         <div className="logo"></div>
 
         <div className="nav-tools">
-          <span>Resultado: {resultado}</span>
-
+          <span>Dinheiro: {dinheiro}</span>
           <div className="user-avatar">NA</div>
         </div>
       </nav>
       <div>
         <PrizeWheel
           ref={wheelRef}
-          sectors={[
-            { id: 1, label: 'Prize 1', value: 1, probability: 10 },
-            { id: 2, label: 'Prize 2', value: 2, probability: 20 },
-            { id: 3, label: 'Prize 3', value: 3, probability: 15 },
-            { id: 4, label: 'Prize 4', value: 4, probability: 5 },
-          ]}
-          onSpinEnd={handleSpinEnd}
+          sectors={sectors}
+          onSpinEnd={(sector) => {
+            console.log('Winner:', sector.label, sector);
+
+            const winner = sector.value
+            setResultado(sector.label);
+            setDinheiro(dinheiro + winner);
+          }}
+          wheelColors={sectors.map((sector) => {
+            if (sector.id % 2 === 0) {
+              return '#131e25'
+            } else {
+              return '#000'
+            }
+          })}
+
+          duration={1}
+
+          frameColor='#eb0000'
+
+          middleColor='#eb0000'
+
+          borderColor='#9e0707'
+
+          sticksColor='#000'
+
+          winIndicatorColor='#f5ab0c'
+
+          winIndicatorDotColor='#000'
+
+          middleDotColor='#000'
         />
       </div>
 
       <MyButton
         onClick={() => {
-          console.log(wheelRef);
-
           wheelRef.current.spin();
-
-          let index = Math.floor(Math.random() * 13);
-
-          let option = optionsWheel[index];
-          setResultado(option);
-
-          setDinheiro(dinheiro + option);
         }}
       />
       <div>
